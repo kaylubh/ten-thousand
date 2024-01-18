8efc7688bf79289228113c15d4c665fc9005c34a
Caleb Hemphill <calebhemp@gmail.com>
2024-01-17 22:00:47 -0500

    Create get input function

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -1,11 +1,30 @@
+def get_input(*valid_input):
+    """
+    
+    """
+
+    response = input("> ")
+    response = response.lower()
+
+    # check response for valid input
+    if response in valid_input:
+        return response
+    else:
+        print("Invalid input. Enter a character in () from above.")
+        get_input(*valid_input)
+
 def welcome():
-    welcome_message = "Welcome to Ten Thousand \n Enter 'q' at any time to exit"
+    """
+    
+    """
+
+    welcome_message = "Welcome to Ten Thousand"
     play_prompt = "(y)es to play or (n)o to decline"
 
     print(welcome_message)
     print(play_prompt)
 
-    response = get_input()
+    response = get_input("y", "n")
 
     if response == "y":
         pass
@@ -13,7 +32,7 @@ def welcome():
         print("OK. Maybe another time")
 
 def play():
-    pass
+    welcome()
 
 
 ################
