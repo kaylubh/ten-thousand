980eb88ca1e7649da00ffd494be74ecfd02c16a0
Caleb Hemphill <calebhemp@gmail.com>
2024-01-18 00:02:15 -0500

    Update game session logic

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -10,7 +10,7 @@ def get_input(*valid_input, dice_input = False):
 
     # handle dice input
     if dice_input:
-        return response
+        return response.replace(" ", "")
 
     # check response for valid input
     if response in valid_input:
@@ -31,26 +31,66 @@ def roll_dice(num_dice):
 
     return rolled_dice_display
 
+def get_score(dice_to_score_string):
+    """
+    
+    """
+
+    dice_to_score_strings = list(dice_to_score_string)
+    dice_to_score_integers = tuple(int(_) for _ in dice_to_score_strings)
+
+    score = GameLogic.calculate_score(dice_to_score_integers)
+
+    return score
+
 def game_session():
     """
     
     """
 
-    round = 0
+    round = 1
     dice = 6
     total_score = 0
+    round_score = 0
+    continue_game = True
 
-    print(f"Starting round {round}")
-    print(f"Rolling {dice} dice...")
-    print(roll_dice(dice))
-    print("Enter dice to keep, or (q)uit:")
+    while continue_game:
+        print(f"Starting round {round}")
+        print(f"Rolling {dice} dice...")
+        print(roll_dice(dice))
+        print("Enter dice to keep, or (q)uit:")
 
-    response = get_input(dice_input = True)
-    
-    if response == "q":
-        print(f"Thanks for playing. You earned {total_score} points")
-    else:
-        pass
+        response = get_input(dice_input = True)
+        
+        if response == "q":
+            print(f"Thanks for playing. You earned {total_score} points")
+
+            continue_game = False
+
+        else:
+            round_score += get_score(response)
+            dice -= len(response)
+
+            print(f"You have {round_score} unbanked points and {dice} dice remaining")
+            print("(r)oll again, (b)ank your points or (q)uit:")
+            
+            continue_response = get_input("r", "b", "q")
+
+            if continue_response == "r":
+                pass
+            elif continue_response == "b":
+                total_score += round_score
+
+                print(f"You banked {round_score} points in round {round}")
+                print(f"Total score is {total_score} points")
+
+                round += 1
+                dice = 6
+                round_score = 0
+            elif continue_response == "q":
+                print(f"Thanks for playing. You earned {total_score} points")
+
+                continue_game = False
 
 def welcome():
     """
