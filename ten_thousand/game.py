c77d897ad82a9f322521d540d314d8159e21c55e
Caleb Hemphill <calebhemp@gmail.com>
2024-01-17 21:37:22 -0500

    Create welcome function

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -1,2 +1,24 @@
+def welcome():
+    welcome_message = "Welcome to Ten Thousand \n Enter 'q' at any time to exit"
+    play_prompt = "(y)es to play or (n)o to decline"
+
+    print(welcome_message)
+    print(play_prompt)
+
+    response = get_input()
+
+    if response == "y":
+        pass
+    elif response == "n":
+        print("OK. Maybe another time")
+
 def play():
     pass
+
+
+################
+## Start Game ##
+################
+
+if __name__ == "__main__":
+    play()
